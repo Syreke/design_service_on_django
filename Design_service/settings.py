99212098a9a7e9998a775077b162ab2f381631c8
Syreke <syreken.ss@gmail.com>
2018-05-02 03:32:19 +0600

    Allowed hosts ozgerdi

--- Design_service/settings.py
+++ Design_service/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['design-service-spring.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['design-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
